92068c0e896942a9bf54e654d2eea3c192c381a2
Sinco <sinconsistencia@gmail.com>
2025-01-28 10:04:58 -0600

    i changed sum stuffs i thinks

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     music.play(music.stringPlayable("C5 - - - - - - - ", 120), music.PlaybackMode.InBackground)
     basic.showLeds(`
         . # . # .
@@ -8,7 +8,7 @@ input.onButtonPressed(Button.A, function () {
         . . . . .
         `)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     music.play(music.stringPlayable("E D G F B A C5 B ", 120), music.PlaybackMode.InBackground)
     for (let index = 0; index < 4; index++) {
         basic.showLeds(`
@@ -36,7 +36,7 @@ input.onButtonPressed(Button.AB, function () {
         . . . . .
         `)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     music.play(music.stringPlayable("C - - - - - - - ", 120), music.PlaybackMode.InBackground)
     basic.showLeds(`
         . # . # .
